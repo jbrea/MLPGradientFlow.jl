0d1c1828bdb87e77188c7f8d41890dec7aaafdd2
Johanni Brea <jbrea@users.noreply.github.com>
2024-01-10 23:19:20 +0100

    num_teach

--- scripts/phase_transition.jl
+++ scripts/phase_transition.jl
@@ -4,17 +4,21 @@ using Distributed
     using MLPGradientFlow, LinearAlgebra, ComponentArrays, OrdinaryDiffEq, Random
 end
 
-configs = collect(Iterators.product(eval(Meta.parse(ARGS[1])), 1:50))
+num_teach = eval(Meta.parse(ARGS[2]))
+configs = collect(Iterators.product(eval(Meta.parse(ARGS[1])),
+                                    1:num_teach,
+                                    (num_teach,)))
 
-@sync @distributed for (seed, num_student) in configs
+@sync @distributed for (seed, num_student, num_teach) in configs
     @show seed num_student
-    param_teach = ComponentVector(w1 = [I(50) zeros(50)], w2 = ones(50))
+    param_teach = ComponentVector(w1 = [I(num_teach) zeros(num_teach)],
+                                  w2 = ones(num_teach))
     rng = Xoshiro(seed)
-    p0 = MLPGradientFlow.glorot(rng, (51, num_student, 1))
+    p0 = MLPGradientFlow.glorot(rng, (num_teach+1, num_student, 1))
     ni = NetI(p0, param_teach, Val(sigmoid2))
     res = train(ni, p0,
                 maxiterations_optim = 0,
                 maxiterations_ode = 10^5,
                 maxtime_ode = 60*60) # 1hr
-    MLPGradientFlow.pickle("erf-stud=$num_student-teach=50-seed=$seed.pkl", res)
+    MLPGradientFlow.pickle("erf-stud=$num_student-teach=$num_teach-seed=$seed.pkl", res)
 end
