3c6a0e1c89fd28b540c0e90fb4a1a3504558d0d9
Johanni Brea <jbrea@users.noreply.github.com>
2024-01-11 09:17:14 +0100

    continue

--- scripts/phase_transition.jl
+++ scripts/phase_transition.jl
@@ -11,14 +11,39 @@ configs = collect(Iterators.product(eval(Meta.parse(ARGS[1])),
 
 @sync @distributed for (seed, num_student, num_teach) in configs
     @show seed num_student
+    fn = "erf-stud=$num_student-teach=$num_teach-seed=$seed.pkl"
     param_teach = ComponentVector(w1 = [I(num_teach) zeros(num_teach)],
                                   w2 = ones(num_teach))
-    rng = Xoshiro(seed)
-    p0 = MLPGradientFlow.glorot(rng, (num_teach+1, num_student, 1))
-    ni = NetI(p0, param_teach, Val(sigmoid2))
-    res = train(ni, p0,
-                maxiterations_optim = 0,
-                maxiterations_ode = 10^5,
-                maxtime_ode = 60*60) # 1hr
-    MLPGradientFlow.pickle("erf-stud=$num_student-teach=$num_teach-seed=$seed.pkl", res)
+    if isfile(fn)
+        res0 = MLPGradientFlow.unpickle(fn)
+        if isnothing(match(r"maxtime", res0["ode_stopped_by"]))
+            continue
+        elseif res0["gnorm"] < 1e-8
+            continue
+        else
+            p0 = MLPGradientFlow.params(res0["x"])
+        end
+        ni = NetI(p0, param_teach, Val(sigmoid2))
+        res = train(ni, p0,
+                    maxiterations_optim = 0,
+                    maxiterations_ode = 10^5,
+                    maxtime_ode = 2*60*60) # 2hr
+        res["init1"] = res["init"]
+        res["init"] = res0["init"]
+        res["loss_curve"] = [res0["loss_curve"]; res["loss_curve"]]
+        ode_t0 = maximum(keys(res0["trajectory"]))
+        for (k, v) in res["trajectory"]
+            res0["trajectory"][k + ode_t0] = v
+        end
+        res["trajectory"] = res0["trajectory"]
+    else
+        rng = Xoshiro(seed)
+        p0 = MLPGradientFlow.glorot(rng, (num_teach+1, num_student, 1))
+        ni = NetI(p0, param_teach, Val(sigmoid2))
+        res = train(ni, p0,
+                    maxiterations_optim = 0,
+                    maxiterations_ode = 10^5,
+                    maxtime_ode = 60*60) # 1hr
+    end
+    MLPGradientFlow.pickle(fn, res)
 end
